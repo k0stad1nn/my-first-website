83b4836b8b58704194a728dd5033afe3c43ba3ee
k0stad1nn <kostadinovicognjen@protonmail.com>
2026-09-04 13:57:46 +0200

    EventListener

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function showGreeting() {
-  alert("Hello, welcome to my website!");
-}
+const myButton = document.getElementById("greetingButton");
+const outputText = document.getElementById("outputText");
+
+myButton.addEventListener("click", () => {
+  outputText.textContent = "Hello, welcome to my first webpage!";
+  outputText.style.color = "#27ae60"; // Change text color to a shade of green
+  outputText.style.fontWeight = "bold"; // Make the text bold
+});
